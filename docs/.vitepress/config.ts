df4beeef148b2bf2f389092d0055ef77028e383c
Robert Soriano <sorianorobertc@gmail.com>
2022-06-21 09:00:16 -0700

    docs: add sidebar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,12 +11,46 @@ export default defineConfig({
       { icon: 'github', link: 'https://github.com/wobsoriano/vuse-gesture' },
     ],
     lastUpdatedText: 'Last Updated',
-    // nav: [
-    //   { text: 'Guide', link: '/', activeMatch: '^/$|^/guide/' },
-    //   { text: 'Playground', link: 'https://lexical-vue-playground.vercel.app' },
-    // ],
+    nav: nav(),
+    sidebar: {
+      '/docs/': sidebarDocs(),
+    },
     footer: {
       message: 'Released under the MIT License.',
     },
   },
 })
+
+function nav() {
+  return [
+    { text: 'Docs', link: '/docs/introduction', activeMatch: '/docs/' },
+    // { text: 'Configs', link: '/config/introduction', activeMatch: '/config/' },
+    // {
+    //   text: 'Changelog',
+    //   link: 'https://github.com/vuejs/vitepress/blob/main/CHANGELOG.md'
+    // }
+  ]
+}
+
+function sidebarDocs() {
+  return [
+    {
+      text: 'About',
+      collapsible: true,
+      items: [
+        { text: 'Introduction', link: '/docs/introduction' },
+        { text: 'Motivation', link: '/docs/motivation' },
+      ],
+    },
+    {
+      text: 'Reference',
+      collapsible: true,
+      items: [
+        { text: 'Gestures', link: '/docs/gestures' },
+        { text: 'Gesture options', link: '/docs/gesture-options' },
+        { text: 'Gesture state', link: '/docs/gesture-state' },
+        { text: 'Utilities', link: '/docs/utilities' },
+      ],
+    },
+  ]
+}
--- docs/index.md
+++ docs/index.md
@@ -11,7 +11,7 @@ hero:
   actions:
     - theme: brand
       text: Get Started
-      link: /about/introduction
+      link: /docs/introduction
     - theme: alt
       text: View on GitHub
       link: https://github.com/wobsoriano/vuse-gesture
