5a17a63d58c457b1e0a8d102ce91c56bf857ea82
Robert Soriano <sorianorobertc@gmail.com>
2022-06-21 07:13:10 -0700

    update playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,36 +1,33 @@
 <script setup lang="ts">
 import { useSpring } from 'vue-use-spring'
 import { normalizeProps, useDrag } from '@wobsoriano/use-gesture'
-import { onMounted } from 'vue'
 
 const position = useSpring({ x: 0, y: 0 })
 
 const bind = useDrag(({ down, movement: [mx, my] }) => {
-  console.log(mx, my, down)
   position.x = down ? mx : 0
   position.y = down ? my : 0
 })
 </script>
 
 <template>
-  <div id="test" v-bind="normalizeProps(bind())" :style="{ touchAction: 'none', transform: `translate(${position.x}px, ${position.y}px)` }">
-    Hello Worlds
-  </div>
+  <div
+    id="box"
+    v-bind="normalizeProps(bind?.())"
+    :style="{ touchAction: 'none', transform: `translate(${position.x}px, ${position.y}px)` }"
+  />
 </template>
 
 <style>
-#app {
+#box {
+  background: skyblue;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-#test {
-  background: red;
   height: 100px;
-  width: 200px;
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
